3465744b18a1bdb82fcfad99dd8d2d87ca5f0cf2
Flavien GAUTIER <flavieng13@gmail.com>
2022-06-01 11:46:42 +0200

    Modification du panel menu et création des scripts

---
+++ Assets/Scripts/AllEvents.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SDD.Events;
+
+
+#region GameManager Events
+public class GameMenuEvent : SDD.Events.Event
+{
+}
+public class GamePlayEvent : SDD.Events.Event
+{
+}
+public class GamePauseEvent : SDD.Events.Event
+{
+}
+public class GameResumeEvent : SDD.Events.Event
+{
+}
+public class GameOverEvent : SDD.Events.Event
+{
+}
+public class GameVictoryEvent : SDD.Events.Event
+{
+}
+public class GameStatisticsChangedEvent : SDD.Events.Event
+{
+	//public int eBestScore { get; set; }
+	public int eScore { get; set; }
+	public float eTimer { get; set; }
+	//public int eNLives { get; set; }
+	//public int eNEnemiesLeftBeforeVictory { get; set; }
+}
+#endregion
+
+#region MenuManager Events
+public class EscapeButtonClickedEvent : SDD.Events.Event
+{
+}
+public class PlayButtonClickedEvent : SDD.Events.Event
+{
+}
+public class ResumeButtonClickedEvent : SDD.Events.Event
+{
+}
+public class ReplayButtonClickedEvent : SDD.Events.Event
+{
+}
+public class MainMenuButtonClickedEvent : SDD.Events.Event
+{
+}
+#endregion
+
+#region Score events
+public class ScoreHasBeenGainedEvent:SDD.Events.Event
+{
+	public int eScore;
+}
+#endregion
